943c49eab9c59bf5661a8cfb4d3404d08f19e064
iansoft <ansoftware@163.com>
2015-08-14 11:08:19 +0800

    delete some files that never used

--- vsmadmin/vsmserver/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns,include,url
-from vsmserver import views
-
-urlpatterns = patterns('',
-                       url(r"^$", views.index, name="vsmserver"),
-                    )
